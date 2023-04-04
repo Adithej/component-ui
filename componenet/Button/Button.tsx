a7fe9bc80cbe21875e970206bf7937d7f7b2c0a4
Adithya Thejas S <adithyathejass.1997@gmail.com>
2023-04-04 17:36:01 +0530

    button-ui

---
+++ componenet/Button/Button.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+interface Props {
+  onClick?: () => void;
+  disabled?: boolean;
+  variant?: "primary" | "secondary" | "danger";
+  icon: React.ReactNode;
+  children: React.ReactNode;
+}
+
+export default function Button({
+  onClick,
+  disabled = false,
+  variant = "primary",
+  icon,
+  children,
+}: Props) {
+  const className =
+    variant === "primary"
+      ? styles.primary
+      : variant === "secondary"
+      ? styles.secondary
+      : styles.danger;
+
+  return (
+    <button
+      className={`${styles.button} ${className}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      <span className={styles.icon}>{icon}</span>
+      <span className={styles.label}>{children}</span>
+    </button>
+  );
+}
