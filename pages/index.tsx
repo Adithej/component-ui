31e27386326418686d02dd928588686471bc3ae5
Adithya Thejas S <adithyathejass.1997@gmail.com>
2023-04-13 18:27:01 +0530

    design-token-naming-2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import Button from "@/componenet/Button/Button";
 // import { useState } from "react";
-import styles from "../componenet/theme.module.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
