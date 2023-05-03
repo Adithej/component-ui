561724f5b51469c09891c81ad654cee331788739
Adithya Thejas S <adithyathejass.1997@gmail.com>
2023-05-03 15:03:12 +0530

    tokens

---
+++ component/Toast/Toast.stories.tsx
@@ -0,0 +1,97 @@
+import { Meta } from "@storybook/react";
+import * as React from "react";
+import * as Toast from "@radix-ui/react-toast";
+import styles from "./Toast.module.scss";
+import Button from "../Button";
+import { X } from "lucide-react";
+import {
+  type ToastProps,
+  type ToastActionElement,
+  ToastProvider,
+  ToastViewport,
+  // Toast,
+  ToastTitle,
+  ToastDescription,
+  ToastClose,
+  ToastAction,
+} from "./Toast";
+
+const meta: Meta<typeof Toast> = {
+  title: "Componenets/Toast",
+  //   component: Toast,
+};
+
+export default meta;
+
+export function ToastDemo() {
+  const [open, setOpen] = React.useState(false);
+  const eventDateRef = React.useRef(new Date());
+  const timerRef = React.useRef(0);
+
+  React.useEffect(() => {
+    return () => clearTimeout(timerRef.current);
+  }, []);
+
+  return (
+    <Toast.Provider swipeDirection="right">
+      <Button
+        variant="secondary"
+        onClick={() => {
+          setOpen(false);
+          window.clearTimeout(timerRef.current);
+          timerRef.current = window.setTimeout(() => {
+            eventDateRef.current = oneWeekAway(11 / 11 / 2022);
+            setOpen(true);
+          }, 100);
+        }}
+        icon={undefined}
+      >
+        Add to calendar
+      </Button>
+
+      <Toast.Root
+        className={styles["ToastRoot"]}
+        open={open}
+        onOpenChange={setOpen}
+      >
+        <Toast.Title className={styles["ToastTitle"]}>
+          Scheduled: Catch up
+        </Toast.Title>
+        <Toast.Description asChild>
+          <time
+            className={styles["ToastDescription"]}
+            dateTime={eventDateRef.current.toISOString()}
+          >
+            {prettyDate(eventDateRef.current)}
+          </time>
+        </Toast.Description>
+        <Toast.Action
+          className={styles["ToastAction"]}
+          asChild
+          altText="Goto schedule to undo"
+        >
+          <div>
+            <X className={styles["toast-close"]} />
+            <Button variant="secondary" icon={undefined}>
+              Undo
+            </Button>
+          </div>
+        </Toast.Action>
+      </Toast.Root>
+      <Toast.Viewport className="ToastViewport" />
+    </Toast.Provider>
+  );
+}
+
+function oneWeekAway(date: number) {
+  const now = new Date();
+  const inOneWeek = now.setDate(now.getDate() + 7);
+  return new Date(inOneWeek);
+}
+
+function prettyDate(date: number | Date | undefined) {
+  return new Intl.DateTimeFormat("en-US", {
+    dateStyle: "full",
+    timeStyle: "short",
+  }).format(date);
+}
